f2b664048ea8dbd73fc15fcf4bd7da7a6ec580fa
bgaleralop <bgaleralop@gmail.com>
2026-06-27 19:42:49 +0200

    feat: Created AccountScreen.kt

---
+++ app/src/main/java/es/bgaleralop/sentinelle/ui/screens/accounts/AccountUiState.kt
@@ -0,0 +1,15 @@
+package es.bgaleralop.sentinelle.ui.screens.accounts
+
+import es.bgaleralop.sentinelle.domain.model.SentinelleAccount
+
+sealed interface AccountUiState {
+    object Loading: AccountUiState
+
+    data class Success(
+        val accounts: List<SentinelleAccount>,
+        val accountCount: Int,
+        val isMaxAccountsReached: Boolean = false
+    ) : AccountUiState
+
+    data class Error(val message: String) : AccountUiState
+}
