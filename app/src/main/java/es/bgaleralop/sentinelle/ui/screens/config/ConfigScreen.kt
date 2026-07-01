7a0a3d3a9f30e1887a5e6f623ea456b86931c017
bgaleralop <bgaleralop@gmail.com>
2026-07-01 13:17:53 +0200

    feat: ConfigScreen.kt added Footer

--- app/src/main/java/es/bgaleralop/sentinelle/ui/screens/config/ConfigScreen.kt
+++ app/src/main/java/es/bgaleralop/sentinelle/ui/screens/config/ConfigScreen.kt
@@ -3,11 +3,15 @@ package es.bgaleralop.sentinelle.ui.screens.config
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -99,6 +103,13 @@ fun ConfigScreen(modifier: Modifier = Modifier) {
                         )
                     }
                 }
+                Spacer(modifier = Modifier.weight(1f))
+                Text(
+                    text = "Sentinelle para Android - v1.0.0(Local first)",
+                    style = MaterialTheme.typography.labelSmall,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
             }
         }
     }
