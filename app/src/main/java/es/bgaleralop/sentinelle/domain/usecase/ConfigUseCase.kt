ec33e680d2a8951b812fcaf9e3114383d837e3ef
bgaleralop <bgaleralop@gmail.com>
2026-07-04 20:37:29 +0200

    feat: Added the use case ConfigUseCase.kt to manage the access to the repository and pass to the viewmodel the data with the business logic over it
    Signed-off-by: bgaleralop <bgaleralop@gmail.com>

---
+++ app/src/main/java/es/bgaleralop/sentinelle/domain/usecase/ConfigUseCase.kt
@@ -0,0 +1,65 @@
+/*
+ *
+ *  Copyright (C) 2026 Sentinelle Team <bgaleralop@gmail.com>
+ *
+ *  This source code is property of Sentinelle Team.
+ *  It is made available publicly for portfolio evaluation and educational purposes only.
+ *  Commercial use, reproduction, or distribution in any digital storefront is
+ *  strictly prohibited under the PolyForm Noncommercial License 1.0.0.
+ *
+ *  For full license details, see the LICENSE.md file in the root directory.
+ *
+ */
+
+package es.bgaleralop.sentinelle.domain.usecase
+
+import es.bgaleralop.sentinelle.domain.repository.UserRepository
+
+/**
+ * @author Bartolomé Galera López (bgaleralop)
+ * @date 02-07-2026
+ *
+ *
+ */
+class ConfigUseCase(
+    private val userRepository: UserRepository,
+) {
+    private var isEmojisFilterActivated: Boolean = false
+    private var isExternalLinksActivated: Boolean = false
+    private var isAdvancedMatchedActivated: Boolean = false
+    private var isDarkMode: Boolean = true
+    fun getUserTier() = userRepository.getUserTier()
+    fun canFilterEmojis(): Boolean {
+        return isEmojisFilterActivated
+    }
+
+    fun canFilterExternalLinks(): Boolean {
+        return isExternalLinksActivated
+    }
+
+    fun canFilterAdvancedMatched(): Boolean {
+        return isAdvancedMatchedActivated
+    }
+
+    fun isDarkMode() = isDarkMode
+
+    fun changeEmojisFilter(): Boolean {
+        isEmojisFilterActivated = !isEmojisFilterActivated
+        return isEmojisFilterActivated
+    }
+
+    fun changeExternalLinksFilter(): Boolean {
+        isExternalLinksActivated = !isExternalLinksActivated
+        return isExternalLinksActivated
+    }
+
+    fun changeAdvanceMatched(): Boolean {
+        isAdvancedMatchedActivated = !isAdvancedMatchedActivated
+        return isAdvancedMatchedActivated
+    }
+
+    fun changeDarkMode(): Boolean {
+        isDarkMode = !isDarkMode
+        return isDarkMode
+    }
+}
