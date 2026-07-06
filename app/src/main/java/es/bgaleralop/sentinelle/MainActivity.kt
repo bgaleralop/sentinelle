2a0aa4d37f55ccea8ce0825156508bc9a966a8ca
bgaleralop <bgaleralop@gmail.com>
2026-07-06 12:36:18 +0200

    refact: MainActivity.kt updated to use MainScreen as the main UI entry point
    Signed-off-by: bgaleralop <bgaleralop@gmail.com>

--- app/src/main/java/es/bgaleralop/sentinelle/MainActivity.kt
+++ app/src/main/java/es/bgaleralop/sentinelle/MainActivity.kt
@@ -17,12 +17,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
-import es.bgaleralop.sentinelle.presentation.screens.dashboard.DashboardScreen
+import es.bgaleralop.sentinelle.presentation.screens.MainScreen
 import es.bgaleralop.sentinelle.presentation.theme.SentinelleTheme
 
 @AndroidEntryPoint
@@ -33,9 +29,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SentinelleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    DashboardScreen(modifier = Modifier.padding(innerPadding))
-                }
+                MainScreen()
             }
         }
     }
