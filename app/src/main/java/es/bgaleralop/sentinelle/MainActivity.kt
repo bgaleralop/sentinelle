4f7b4e1def0e92439bd1b8cbb32ae7ba9ce00a57
bgaleralop <bgaleralop@gmail.com>
2026-07-06 11:03:54 +0200

    feat: MainActivity.kt updated to display DashboardScreen instead of ConfigScreen and removed ConfigViewModel reference.
    Signed-off-by: bgaleralop <bgaleralop@gmail.com>

--- app/src/main/java/es/bgaleralop/sentinelle/MainActivity.kt
+++ app/src/main/java/es/bgaleralop/sentinelle/MainActivity.kt
@@ -17,27 +17,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import dagger.hilt.android.AndroidEntryPoint
-import es.bgaleralop.sentinelle.presentation.screens.config.ConfigScreen
-import es.bgaleralop.sentinelle.presentation.screens.config.ConfigViewModel
+import es.bgaleralop.sentinelle.presentation.screens.dashboard.DashboardScreen
 import es.bgaleralop.sentinelle.presentation.theme.SentinelleTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val viewModel by viewModels<ConfigViewModel>()
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SentinelleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ConfigScreen(viewModel, modifier = Modifier.padding(innerPadding))
+                    DashboardScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
