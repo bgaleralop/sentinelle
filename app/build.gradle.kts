192812d8b1a13af016def70c870480ae8cdfaff4
bgaleralop <bgaleralop@gmail.com>
2026-07-06 10:48:56 +0200

    feat: app/build.gradle.kts added Room compiler dependency via KSP
    Signed-off-by: bgaleralop <bgaleralop@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     // Room
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
     // Hilt
     implementation(libs.hilt.android)
     implementation(libs.androidx.hilt.navigation.compose)
