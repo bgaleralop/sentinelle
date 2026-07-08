be9fb13c88aa74794e4f2c9ea932d003865a5f9b
bgaleralop <bgaleralop@gmail.com>
2026-07-08 17:16:15 +0200

    refactor: Moved MyTopAppBar to his own file and renamed to MainTopAppBar.kt
    Signed-off-by: bgaleralop <bgaleralop@gmail.com>

--- app/src/main/java/es/bgaleralop/sentinelle/presentation/screens/commons/SmallTopAppBar.kt
+++ app/src/main/java/es/bgaleralop/sentinelle/presentation/screens/commons/SmallTopAppBar.kt
@@ -13,25 +13,15 @@
 
 package es.bgaleralop.sentinelle.presentation.screens.commons
 
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 
 /**
  * @author Bartolomé Galera López (bgaleralop)
@@ -60,38 +50,3 @@ fun SmallTopAppBar(
     )
 }
 
-@Composable
-fun MyTopAppBar(modifier: Modifier = Modifier) {
-    SmallTopAppBar(
-        title = {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(
-                    text = "Sentinelle",
-                    style = MaterialTheme.typography.titleLarge,
-                    fontWeight = FontWeight.ExtraBold
-                )
-                Spacer(
-                    modifier = Modifier
-                        .width(16.dp)
-                        .weight(0.5f)
-                )
-                Surface(
-                    modifier = Modifier.padding(bottom = 4.dp, end = 16.dp),
-                    color = Color(0xFF00C9A7).copy(alpha = 0.15f),
-                    shape = RoundedCornerShape(16.dp)
-                ) {
-                    Text(
-                        text = "Sistema Activo",
-                        modifier = Modifier.padding(horizontal = 12.dp, vertical = 6.dp),
-                        style = MaterialTheme.typography.labelMedium,
-                        color = Color(0xFF00C9A7)
-                    )
-                }
-            }
-        },
-        colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color.Transparent,
-            titleContentColor = MaterialTheme.colorScheme.onBackground
-        )
-    )
-}
