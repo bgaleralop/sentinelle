9f074c492213b778f8ea0f04b4737e57921d1842
bgaleralop <bgaleralop@gmail.com>
2026-07-14 10:55:04 +0200

    refact: Moved MySearchBar.kt to his own file in screens/commons
    Signed-off-by: bgaleralop <bgaleralop@gmail.com>

---
+++ app/src/main/java/es/bgaleralop/sentinelle/presentation/screens/commons/MySearchBar.kt
@@ -0,0 +1,58 @@
+/*
+ *
+ *  Copyright (C) 2026 Sentinelle Team <bgaleralop@gmail.com>
+ *
+ *  This source code is property of Sentinelle Team.
+ *  It is made available publicly for portfolio evaluation and educational purposes only.
+ *  Commercial use, reproduction, or distribution in any digital storefront is
+ *  strictly prohibited under the PolyForm Noncommercial License 1.0.0.
+ *
+ *  For full license details, see the LICENSE.md file in the root directory.
+ *
+ */
+
+package es.bgaleralop.sentinelle.presentation.screens.commons
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MySearchBar(
+    query: String,
+    placeholder: String,
+    onUpdate: (String) -> Unit,
+    onSubmit: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    OutlinedTextField(
+        value = query,
+        onValueChange = { onUpdate(it) },
+        placeholder = { Text(text = placeholder) },
+        singleLine = true,
+        maxLines = 1,
+        shape = ShapeDefaults.ExtraLarge,
+        suffix = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                null,
+                modifier = Modifier.clickable(
+                    enabled = true,
+                    onClick = { onSubmit() }
+                )
+            )
+        },
+        colors = TextFieldDefaults.colors(),
+        modifier = modifier.fillMaxWidth().padding(horizontal = 16.dp)
+    )
+}
