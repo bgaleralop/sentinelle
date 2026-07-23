ad6af0cb1108e7656b1693dc85346137a6f0dbe1
bgaleralop <bgaleralop@gmail.com>
2026-07-23 08:34:51 +0200

    feat: Added BlacklistUsersScreen.kt, updated DashboardScreen.kt to use the passed modifier and fakeDashboardSuccessState.
    Signed-off-by: bgaleralop <bgaleralop@gmail.com>

---
+++ app/src/main/java/es/bgaleralop/sentinelle/presentation/screens/blacklist/components/BlacklistUsersScreen.kt
@@ -0,0 +1,27 @@
+/*
+ *
+ *  Copyright (C) 2026 Sentinelle Team <bgaleralop@gmail.com>
+ *
+ *  This source code is property of Sentinelle Team.
+ *  It is made available publicly for portfolio evaluation and educational purposes only.
+ *  Commercial use, reproduction, or distribution in any digital storefront is
+ *  strictly prohibited under the PolyForm Noncommercial License 1.0.0.
+ *
+ *  For full license details, see the LICENSE.md file in the root directory.
+ *
+ */
+
+package es.bgaleralop.sentinelle.presentation.screens.blacklist.components
+
+import androidx.compose.runtime.Composable
+
+/**
+ * @author Bartolomé Galera López (bgaleralop)
+ * @date 13-07-2026
+ *
+ * Composable que renderiza la pantalla de usuarios baneados dentro de Lista Negra.
+ */
+@Composable
+fun BlacklistUsersScreen() {
+
+}
